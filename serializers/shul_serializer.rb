63bc2fdfd0b33ca89a4baa90573886b6c8345000
ryan horowitz <ryan.w.horowitz@gmail.com>
2023-09-20 16:28:29 -0400

    deleted many files that were dups and in the wrong level of architecture. Baruch HaShem!

--- serializers/shul_serializer.rb
+++
@@ -1,5 +0,0 @@
-class ShulSerializer < ActiveModel::Serializer
-  attributes :id, :name, :street_address, :city, :state,
-             :postal_code, :img, :contact_email
-  # maybe make a custom address Serializer
-end
--- serializers/user_serializer.rb
+++
@@ -1,5 +0,0 @@
-class UserSerializer < ActiveModel::Serializer
-  attributes :id, :username, :email
-  has_many :services
-  has_many :user_services
-end
