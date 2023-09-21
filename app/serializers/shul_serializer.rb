810bdfce0c8c063258c9a86af788e08947d6940d
ryan horowitz <ryan.w.horowitz@gmail.com>
2023-09-21 15:05:01 -0400

    deleted ShulServiceSerializer.rb and assciated spec file.

--- app/serializers/shul_serializer.rb
+++ app/serializers/shul_serializer.rb
@@ -11,23 +11,4 @@ class ShulSerializer < ActiveModel::Serializer
   # end
 
   # maybe make a custom address Serializer
-
-  # def next_service
-  #   # byebug
-  #   puts 'in next service'
-  #   next_service = object.services.last
-  #   next_service[:formated_time] = '7:15'
-  # end
-
-  # def parsed_time
-  #   object.time.strftime('%I:%M %p')
-  #         .sub('AM', 'a.m.') # sub out AM for a.m
-  #         .sub('PM', 'p.m.')
-  #         .gsub(/^0/, '') # gets rid of leading 0
-  # end
-
-  # def parsed_date
-  #   Date.parse(object.date.to_s)
-  #       .strftime('%a %B %e, %Y')
-  # end
 end
--- app/serializers/shul_service_serializer.rb
+++
@@ -1,9 +0,0 @@
-class ShulServiceSerializer < ActiveModel::Serializer
-  attributes :id, :name, :street_address, :city,
-             :state, :postal_code, :img, :next_service
-  # has_many :services
-  def next_service
-    # byebug
-    next_service = object.services.last
-  end
-end
