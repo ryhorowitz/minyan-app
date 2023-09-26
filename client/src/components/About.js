bd793e348584943ebf5b03ee128ea3901decb537
ryan horowitz <ryan.w.horowitz@gmail.com>
2023-09-26 17:52:01 -0400

    About.js added text

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -2,7 +2,11 @@ function About() {
 
   return (
     <>
-      ABOUT
+      <div className="container mt-4 text-center">
+        <h2>Minyan Maker</h2>
+        <p className="text-body">an app for you to rsvp for a minyan in the Center-City Philly area.</p>
+      </div>
+
     </>
   )
 }
