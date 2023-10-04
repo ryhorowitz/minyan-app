8a6c4519ebac381a80db684a2b5b07859bcf60d3
ryan horowitz <ryan.w.horowitz@gmail.com>
2023-10-04 12:37:02 -0400

    wrote rescue block in contact_shul_controller.rb

--- app/controllers/contact_shul_controller.rb
+++ app/controllers/contact_shul_controller.rb
@@ -6,9 +6,10 @@ class ContactShulController < ApplicationController
     # byebug
     begin
       ContactShulMailer.send_email(recipient, message, sender).deliver_now
-      render json: { 'message': 'message sent' }, status: :ok
-    rescue StandardError
-      flash[:error] = 'There was an error sending the email. Please try again later.'
+      render json: { 'message': 'contact email successfully sent' }, status: :ok
+    rescue StandardError e
+      logger.error "An error occurred: #{e.message}"
+      render json: { error: "There was an error sending the email. Please try again later." }, status: :internal_server_error
     end
   end
 end
