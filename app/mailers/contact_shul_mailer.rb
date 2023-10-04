e8e68b68b98fa9c7621ad48ecad051a25df5fb18
ryan horowitz <ryan.w.horowitz@gmail.com>
2023-10-04 12:54:23 -0400

    wrote rescue block in contact_shul_mailerer.rb

--- app/mailers/contact_shul_mailer.rb
+++ app/mailers/contact_shul_mailer.rb
@@ -7,5 +7,8 @@ class ContactShulMailer < ApplicationMailer
       to: @recipient,
       subject: "New message from #{@sender.username}"
     )
+  rescue StandardError => e
+    flash[:error] = 'There was an error sending the email from ContactShulMailer. Please try again later.'
+    redirect_to error_path
   end
 end
